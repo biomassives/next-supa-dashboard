60da6393d6d0c37868ff196c214b683b528d5165
G. Willson <greg@ecocity.com>
2024-11-12 14:13:44 -0700

    ui error o fix

--- app/dashboard/components/app-bar.tsx
+++ app/dashboard/components/app-bar.tsx
@@ -1,50 +1,31 @@
 // @/app/dashboard/components/app-bar.tsx
+'use client'
+
 import * as React from 'react'
-import { type User } from '@supabase/supabase-js'
-import { cn } from '@/lib/utils'
-import { MobileNav } from '@/app/dashboard/components/mobile-nav'
-import { MainNav } from '@/app/dashboard/components/main-nav'
-import { Profile } from '@/app/dashboard/components/profile'
-import { LanguageToggle } from '@/app/dashboard/components/language-toggle'
-import { ThemeToggle } from '@/app/dashboard/components/theme-toggle'
+import { AccountMenu } from '@/components/account-menu'
+import { SiteBrand } from '@/components/site-brand'
 import { Notify } from '@/app/dashboard/components/notify'
-import type { Notification } from '@/types/notification'
+import { cn } from '@/lib/utils'
 
-interface AppBarProps extends React.HTMLAttributes<HTMLDivElement> {
-  user?: User | null
-  notification?: Notification | null
-}
+interface AppBarProps extends React.HTMLAttributes<HTMLElement> {}
 
-export function AppBar({
-  user,
-  notification,
-  className,
-  ...props
-}: AppBarProps) {
+const AppBar = ({ children, className, ...props }: AppBarProps) => {
   return (
     <header
       className={cn(
-        'supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur',
+        'flex w-full items-center gap-4 border-b bg-background px-4',
+        'h-[60px]',
         className
       )}
       {...props}
     >
-      <div className="container flex h-14 items-center">
-        <MainNav />
-        <MobileNav />
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-            {/* search or other content */}
-          </div>
-          <div className="flex items-center gap-2">
-            <LanguageToggle />
-            <ThemeToggle />
-            <Notify notification={notification} />
-            <Profile user={user} />
-          </div>
-        </div>
-      </div>
+      <SiteBrand />
+      {children}
+      <div className="flex-1 text-destructive"></div>
+      <Notify />
+      <AccountMenu />
     </header>
   )
 }
 
+export { AppBar, type AppBarProps }
